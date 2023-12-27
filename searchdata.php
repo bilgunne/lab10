af8c441c9aef3dd95c5e13036f936975fc13de57
w3school-test <test@w3school.com>
2023-12-27 17:10:50 +0800

    initialcommit

--- searchdata.php
+++ searchdata.php
@@ -10,3 +10,7 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$parameter = isset($_GET['parameter']) ? $_GET['parameter'] : '';
+
+$sql = "SELECT h.size as size, s.area as area, h.itemname as itemname, r.sectionname as sectionname, il.quantity as quantity, il.condition1 as condition1 FROM itemlocation il inner join storeholditems s on s.itemNo = il.itemno inner join section se on se.sectionNo = il.sectionNo WHERE il.sectionNo = ?";
