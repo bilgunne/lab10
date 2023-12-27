a4e83490f912d5d9f0ecf82bf9153815c72b9a8a
w3school-test <test@w3school.com>
2023-12-27 17:09:03 +0800

    initialcommit

--- searchdata.php
+++ searchdata.php
@@ -4,3 +4,9 @@ $servername = "localhost";
 $username = "b";
 $password = "";
 $dbname = "database_lab10";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
