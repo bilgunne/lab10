eacdccb46bdb1f6efca444d9557d9623f8026469
w3school-test <test@w3school.com>
2023-12-27 17:00:41 +0800

    initialcommit

---
+++ detail.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <h2>Тасгийн мэдээллийг харах хуудас.</h2>
+
+    <label>Хайх утгыг оруулна уу?</label> <br>
+    <label>Жишээлбэл: HI001-HI050, S01-S08</label> <br>
+    <input type="character" id="parameterinput">
+    <button onclick="activatephp()">Хайх</button>
+
+    <div id="result"></div>
+
+
+</body>
+</html>
